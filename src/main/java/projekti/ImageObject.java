368fb3ce4c1206e787d24ae89d3325c2a9f57784
tapanih <tapani.honkanen@hotmail.com>
2019-05-05 21:02:23 +0300

    muutettu Lob-annotaatio Type-annotaatioksi, jotta se toimisi Herokussa

--- src/main/java/projekti/ImageObject.java
+++ src/main/java/projekti/ImageObject.java
@@ -12,11 +12,11 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @NoArgsConstructor
@@ -25,8 +25,9 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Entity
 public class ImageObject extends AbstractPersistable<Long> {
         
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
+    //@Lob
+    //@Basic(fetch = FetchType.LAZY)
+    @Type(type = "org.hibernate.type.BinaryType")
     private byte[] content;
     @Column(length=65000)    
     private String description;
